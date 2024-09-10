e5549e61d38bf802f8c2364712f6360b0c03e93b
SuleymanAli <suleymanali76@gmail.com>
2024-09-10 20:08:43 +0400

    Assets & Typography Set Up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
+        serif: ['Integral CF', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
